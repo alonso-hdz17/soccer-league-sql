c83c8d265af2bd9288ba601f376b447151512aa0
alonso-hdz17 <alonsohdz0783@gmail.com>
2023-06-05 17:52:45 -0500

    Added Players entity.

--- create.sql
+++ create.sql
@@ -1,1 +1,11 @@
 CREATE TABLE TEAMS(team_id int PRIMARY KEY(), team_name varchar(32));
+
+CREATE TABLE PLAYERS(
+  player_id int PRIMARY KEY()
+  , first_name varchar(20)
+  , last_name varchar(20)
+  , position varchar(10)
+  , nationality varchar(32)
+  , age int
+  , team_id FOREIGN KEY
+);
